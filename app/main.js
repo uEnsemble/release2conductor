97a764e9362997e089d53080407b8a20cd6e4b13
Travis CI <builds@travis-ci.com>
2017-01-10 13:28:02 -0500

    print api url

--- app/main.js
+++ app/main.js
@@ -16,7 +16,8 @@ var PORT = appEnv.port || 3003;
 if(!appEnv.WEBHOOK_KEY)
   appEnv.WEBHOOK_KEY = 'abcdefg';
 
-log.trace('process.env===', process.env);
+//log.trace('process.env===', process.env);
+log.trace('process.env.CONDUCTOR_API===', process.env.CONDUCTOR_API);
 
 //Create express server & socket.io
 var app = express();
